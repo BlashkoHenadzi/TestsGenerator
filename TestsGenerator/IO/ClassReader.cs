3d6dd407fa7f59543e42788cb28e72f9562e8f94
BlashkoHenadzi <47740678+BlashkoHenadzi@users.noreply.github.com>
2019-11-18 12:15:50 +0300

    IO added;

---
+++ TestsGenerator/IO/ClassReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator.IO
+{
+    public class ClassReader
+    {
+        public static async Task<string> Read(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return await reader.ReadToEndAsync();
+            }
+
+        }
+    }
+}
---
+++ TestsGenerator/IO/ClassWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGenerator.IO
+{
+    public class ClassWriter
+    {
+        private static  void CheckDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);//TODO:cath exceprions;
+
+        }
+
+        public static async Task Write(string path, List<ClassInfo> generatedClasses)
+        {
+            CheckDirectory(path);
+            foreach (ClassInfo classInfo in generatedClasses)
+            {
+                using (StreamWriter writer = new StreamWriter(Path.Combine(path, "\\", classInfo.name)))
+                {
+                    await writer.WriteAsync(classInfo.Body);
+                }
+            }
+
+        }
+        
+    }
+}
